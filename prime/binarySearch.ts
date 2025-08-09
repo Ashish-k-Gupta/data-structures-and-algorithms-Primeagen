594b09bf38b9ce571a1a9725fa44e788fba705ea
Ashish Gupta <theashukumar007@gmail.com>
2025-08-10 00:33:48 +0530

    fixed how we were getting average the old method can overflow integer in some languages

--- prime/binarySearch.ts
+++ prime/binarySearch.ts
@@ -4,7 +4,7 @@ export default function bs_list(haystack: number[], needle: number): boolean{
     let high = haystack.length - 1;
     let current;
     do{
-        current = Math.floor((low + high)/2);
+        current = Math.floor(low + (high-low)/2);
         if(haystack[current] > needle){
             high = current - 1;
         }else if(haystack[current]< needle){
