185e8e942023018a2f1fcd66472aad7668d6e2a8
Ashish Gupta <theashukumar007@gmail.com>
2025-08-10 19:48:34 +0530

    re wrote

--- prime/binarySearch.ts
+++ prime/binarySearch.ts
@@ -1,18 +1,19 @@
-export default function bs_list(haystack: number[], needle: number): boolean{
-
+export default function bindary_search(haystack: number[], needle: number): boolean{
     let low = 0;
-    let high = haystack.length - 1;
-    let current;
+    let high = haystack.length-1;
+    let middle;
+
     do{
-        current = Math.floor(low + (high-low)/2);
-        if(haystack[current] > needle){
-            high = current - 1;
-        }else if(haystack[current]< needle){
-            low = current + 1;
+        middle = Math.floor((low + (high - low))/2)
+        if(haystack[middle] > needle){
+            high = middle - 1;
+        }else if(haystack[middle] < needle){
+            low = middle + 1;
         }else{
             return true;
         }
-    }
-    while(low <=high)
+
+    }while(low <= high)
+
         return false;
 }
