c3725ba2212ee7d1cfeb7c6a36eb9feee3d33350
Ashish Gupta <theashukumar007@gmail.com>
2025-08-10 21:58:55 +0530

    solved again

--- prime/bubbleSort.ts
+++ prime/bubbleSort.ts
@@ -1,13 +1,11 @@
-export default function bubble_sort(numArray: number[]): void {
-
-    for (let i = 0; i < numArray.length; ++i) {
-        for (let j = 0; j < numArray.length - 1 - i; ++j) {
-
-            if (numArray[j] > numArray[j + 1]) {
-                let temp = numArray[j];
-                numArray[j] = numArray[j + 1];
-                numArray[j + 1] = temp;
+export default function bubble_sort(numAarray : number[]): void{
+    for(let i = 0; i < numAarray.length; i++){
+        for(let j = 0; j < numAarray.length - 1 - i; j++){
+            if(numAarray[j] > numAarray[j + 1]){
+                const temp = numAarray[j];
+                numAarray[j] = numAarray[j + 1];
+                numAarray[j+1]  = temp;
             }
         }
     }
-} 
+}
